c026c46c86ac1682ff9bc37a5b002bbcf4efe806
KarlaDominguez <78819236+KarlaDominguez@users.noreply.github.com>
2021-03-19 20:19:57 -0600

    Scripts  PowerShell
    una variable
    un if
    un ciclo
    una funcion con parametro

---
+++ ScripPowerShell.ps1
@@ -0,0 +1,27 @@
+﻿[int]$valor1 = Read-Host "valor 1"
+[int]$valor2 = Read-Host "valor 2"
+
+Function Get-Suma {
+param(
+$N1, $N2
+)
+
+$Resultado = $N1 + $N2
+Write-host 'Resultado de la suma' $Resultado
+}
+
+Get-Suma -N1 $valor1 -N2 $valor2
+if ( $valor1 -match $valor2 ){
+    echo ''
+    Write-host "valor1 y valor2 son los mismos "
+}
+echo ''
+echo 'Tabla de multiplicar del primer valor, del 1 al 10'
+$val=0
+while($val -ne 10)
+{
+    $val++
+    $x =($val*$valor1)
+    Write-host $valor1 '*' $val' = '$x
+  
+}
